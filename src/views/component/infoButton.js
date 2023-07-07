7dfb634291c9edcb02ce71a534826c36eae39400
joonmoogo <junemuk10@gmail.com>
2023-07-07 20:18:04 +0900

    info_button : button to popup

--- src/views/component/infoButton.js
+++ src/views/component/infoButton.js
@@ -1,20 +1,35 @@
-import { Accordion, Rail, Icon, Comment, Table, List, Image as ImageComponent, Item, Card, Menu, Message, Grid, Header, Button, Form, Segment, Image, Container, TableRow } from "semantic-ui-react";
+import { Rating,Popup,Accordion, Rail, Icon, Comment, Table, List, Image as ImageComponent, Item, Card, Menu, Message, Grid, Header, Button, Form, Segment, Image, Container, TableRow } from "semantic-ui-react";
 
 function InfoButton(){
     return(
-        <Button style={{ 
+        <div style={{}}>
+        <Popup trigger={<Button style={{ 
             top:'90%',
             left:'90%',
             position: 'absolute',
             backgroundColor:'red'}}
-            size="big"
+            size="large"
             circular 
             color='google plus'
             icon='tasks' 
             onClick={()=>{
                 console.log('infoButton was clicked');
-            }}/>
+            }}/>}
+            content={<>
+            <List>⁘ some task is here Check me </List>
+            <List>⁘ some task is here Check me </List>
+            <List>⁘ some task is here Check me </List>
+            <List>⁘ some task is here Check me </List>
+            <List>⁘ some task is here Check me </List>
+            <List>⁘ some task is here Check me </List>
+            <List>⁘ some task is here Check me </List>
+            <List>⁘ some task is here Check me </List>
+            </>}
+            />
+           
+            </div>
     )
 }
+
 
 export default InfoButton;
