ea24b9bb0637d24f9bb01a8f98f4642c2890a733
joonmoogo <junemuk10@gmail.com>
2023-07-17 07:46:10 +0900

    divide : a shit => two shit

--- src/utils/ReceiptUtil.ts
+++ src/utils/ReceiptUtil.ts
@@ -51,67 +51,7 @@ export default class ReceiptUtil{
       }
 }
 
-class ChartUtil {
-    
-    static getSalesTotal(array : string[][]): number {
-        let total : number = 0;
-        array.map((innerArray : string[])=>{
-            innerArray.map((e : any)=>{
-                total += (parseInt(e.count) * parseInt(e.price))
-            })
-        })
-        return total;
-    }
-  
-    static getAverage(array: string[][]): number {
-        let total: number = 0;
-        let count: number = 0;
-      
-        array.forEach((innerArray: string[]) => {
-          innerArray.forEach((e: any) => {
-            total += parseInt(e.count) * parseInt(e.price);
-            count++;
-          });
-        });
-      
-        if (count === 0) {
-          return 0;
-        }
-      
-        const average: number = total / count;
-        return average;
-      }
-  
-      static getMaxVal(array: string[][]): number {
-        let maxVal: number = Number.NEGATIVE_INFINITY;
-      
-        array.forEach((innerArray: string[]) => {
-          innerArray.forEach((e: any) => {
-            const val: number = parseInt(e.count) * parseInt(e.price);
-            if (val > maxVal) {
-              maxVal = val;
-            }
-          });
-        });
-      
-        return maxVal;
-      }
-      
-      static getMinVal(array: string[][]): number {
-        let minVal: number = Number.POSITIVE_INFINITY;
-      
-        array.forEach((innerArray: string[]) => {
-          innerArray.forEach((e: any) => {
-            const val: number = parseInt(e.count) * parseInt(e.price);
-            if (val < minVal) {
-              minVal = val;
-            }
-          });
-        });
-      
-        return minVal;
-      }
-  }
+
 
 // function 매출 구하기 ( 날짜 ) return '날짜'의 매출
 // function 매출 평균 구하기 (시작 날짜, 종료 날짜) return 평균
